698d6fe24eb8a83f6a3f599bcdc862464fa36b32
Shawn Esquivel <shawnesquivel24@gmail.com>
2024-04-22 11:00:00 -0700

    increase emoji size

--- src/app/_components/Title.jsx
+++ src/app/_components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ emoji, headingText }) => {
   return (
     <>
-      <p className="text-center mb-4">{emoji}</p>
+      <p className="text-center mb-4 text-2xl">{emoji}</p>
       <p className="text-center mb-8">{headingText.toUpperCase()}</p>
     </>
   );
